e5b286eeb4a4fc8423b6555af6f133464fa3bec9
José Ygor Dantas <jose.dantas@ever.com.br>
2018-08-18 10:08:16 -0300

    Added models User, Post and Comment + install sequelize and postgreSQL

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -22,11 +22,27 @@ const typeDefs = `
   type Query {
     allUsers: [User!]!
   }
+
+  type Mutation {
+    createUser(name: String!, email: String!): User
+  }
 `
 
 const resolvers = {
+  User: {
+    id: user => user.id,
+    name: user => user.name,
+    email: user => user.email
+  },
   Query: {
     allUsers: () => users
+  },
+  Mutation: {
+    createUser: (parent, args) => {
+      const newUser = Object.assign({id: users.length+1}, args)
+      users.push(newUser)
+      return newUser
+    } 
   }
 }
 
---
+++ src/interfaces/ModelsInterface.ts
@@ -0,0 +1,9 @@
+import { CommentModel } from "../models/CommentModel";
+import { PostModel } from "../models/PostModel";
+import { UserModel } from "../models/UserModel";
+
+export interface ModelsInterface {
+  Comment: CommentModel;
+  Post: PostModel;
+  User: UserModel; 
+}
